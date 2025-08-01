e16ee03eafe7e5896c459b8b66007e6b8ef9febd
badrinath-dash <badrinath.dash@gmail.com>
2025-08-01 17:06:35 +1000

    Update SplunkAPIAccessPage.jsx

--- SplunkAPIAccessPage.jsx
+++ SplunkAPIAccessPage.jsx
@@ -9,6 +9,7 @@ import ApplicationDetailsForm from '../components/ApiAccessPage/ApplicationDetai
 import DataClassificationForm from '../components/ApiAccessPage/DataClassificationForm';
 import SplunkDetailsForm from '../components/ApiAccessPage/SplunkDetailsForm';
 import TermsAndConditionForm from '../components/ApiAccessPage/TermsAndConditionForm';
+import FinanceDetailsForm from '../components/ApiAccessPage/FinanceForm'
 import Info from '../components/ApiAccessPage/CostInfo';
 import Icon from '../components/Icon';
 
@@ -16,6 +17,7 @@ const steps = [
   'Application Details',
   'Data Classification',
   'Splunk Details',
+  'Finance Details',
   'Terms & Conditions'
 ];
 
@@ -29,7 +31,18 @@ export default function SplunkRestAPIAccess() {
     dataOwner: '',
     techContact: '',
     supportContact: '',
-    currentUser: ''
+    currentUser: '',
+    sensitiveInfoFlag: '',
+    PIIDataFlag: '',
+    complianceArtefact: '',
+    targetSystemDataRetention: '',
+    standardAPIRequestType: '',
+    nonStandardAPIRequestType: '',
+    restRobotAccountUsed: '',
+    restQueryFrequency: '',
+    infrastrcurePMOCode: '',
+    laborPMOCode: '',
+    termsAccepted: false,
   });
 
   const [applicationFormErrors, setApplicationFormErrors] = useState({});
@@ -38,22 +51,90 @@ export default function SplunkRestAPIAccess() {
     setActiveStepId((prev) => Math.max(prev - 1, 0));
   }, []);
 
-  const handleNext = useCallback(() => {
-    if (activeStepId === 0) {
-      if (validateApplicationDetailsStep()) {
-        setActiveStepId((prev) => Math.min(prev + 1, numSteps - 1));
-      }
-    } else {
-      setActiveStepId((prev) => Math.min(prev + 1, numSteps - 1));
+  const validateAPIFormSteps = (stepId) => {
+    switch (stepId) {
+      // Below has been commented temporarily to bypass field validation during testing
+      // case 0:
+      //   return validateApplicationDetailsStep();
+      // case 1:
+      //   return ValidateDataClassificationForm();
+      default:
+        return true;
     }
-  }, [activeStepId, numSteps, validateApplicationDetailsStep]);
+  }
+
+  const handleNext = useCallback(() => {
+    if (validateAPIFormSteps(activeStepId)) {
+      setActiveStepId((prev) => Math.min(prev + 1, numSteps - 1));
+    };
+
+  }, [activeStepId, numSteps, validateAPIFormSteps]);
 
   const validateApplicationDetailsStep = () => {
     const newErrors = {};
-    if (!applicationFormData.appID) newErrors.appID = 'Application is required';
-    if (!applicationFormData.businessJustification) newErrors.businessJustification = 'Business justification is required';
-    if (!applicationFormData.techContact) newErrors.techContact = 'Technical contact is required';
-    if (!applicationFormData.supportContact) newErrors.supportContact = 'Support contact is required';
+    const telstraEmailRegex = /^[^\s@]+@team\.telstra\.com$/;
+    if (!applicationFormData.appID) {
+      newErrors.appID = 'Application is required';
+    }
+    if (!applicationFormData.businessJustification) {
+      newErrors.businessJustification = 'Business justification is required';
+    }
+    if (!applicationFormData.techContact) {
+      newErrors.techContact = 'Technical contact is required';
+    } else if (!telstraEmailRegex.test(applicationFormData.techContact)) {
+      newErrors.techContact = 'Tecnical contact must be a valid Telstra email address ending with telstra.com'
+    }
+    if (!applicationFormData.supportContact) {
+      newErrors.supportContact = 'Support contact is required';
+    } else if (!telstraEmailRegex.test(applicationFormData.supportContact)) {
+      newErrors.supportContact = 'Support contact must be a valid Telstra email address ending with telstra.com'
+    }
+
+    setApplicationFormErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+
+
+
+
+  const calculateCost = useCallback(() => {
+    let baseCost = 100;
+    baseCost += (1440 / applicationFormData.restQueryFrequency) * 10;
+
+
+
+    if (applicationFormData.standardAPIRequestType === 'Yes') {
+      baseCost += 0;
+    }
+
+    if (applicationFormData.nonStandardAPIRequestType === 'Yes') {
+      baseCost += 1000;
+    }
+
+    return `$${baseCost.toFixed(2)}`;
+  }, [applicationFormData]);
+
+
+
+  const ValidateDataClassificationForm = () => {
+    const newErrors = {};
+    if (!applicationFormData.sensitiveInfoFlag) {
+      newErrors.sensitiveInfoFlag = 'Please select if requested dataset contains Sensitive Information';
+    }
+    if (!applicationFormData.PIIDataFlag) {
+      newErrors.PIIDataFlag = 'Please select if requested dataset PII Information';
+    }
+    if (!applicationFormData.complianceArtefact) {
+      newErrors.complianceArtefact = 'Compliance Artefact is mandatory';
+    }
+    if (!applicationFormData.targetSystemDataRetention) {
+      newErrors.targetSystemDataRetention = 'Please specify Target system Data retention ';
+    }
+    if (!applicationFormData.dataEncryptionRestFlag) {
+      newErrors.dataEncryptionRestFlag = 'Please specify data retention at rest ';
+    }
+
     setApplicationFormErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
@@ -85,7 +166,7 @@ export default function SplunkRestAPIAccess() {
           <ApplicationDetailsForm
             applicationFormData={applicationFormData}
             setApplicationFormData={setApplicationFormData}
-            errors={applicationFormErrors}
+            applicationFormErrors={applicationFormErrors}
           />
         );
       case 1:
@@ -93,6 +174,7 @@ export default function SplunkRestAPIAccess() {
           <DataClassificationForm
             applicationFormData={applicationFormData}
             setApplicationFormData={setApplicationFormData}
+            applicationFormErrors={applicationFormErrors}
           />
         );
       case 2:
@@ -100,13 +182,23 @@ export default function SplunkRestAPIAccess() {
           <SplunkDetailsForm
             applicationFormData={applicationFormData}
             setApplicationFormData={setApplicationFormData}
+            applicationFormErrors={applicationFormErrors}
           />
         );
       case 3:
         return (
+          <FinanceDetailsForm
+            applicationFormData={applicationFormData}
+            setApplicationFormData={setApplicationFormData}
+            applicationFormErrors={applicationFormErrors}
+          />
+        );
+      case 4:
+        return (
           <TermsAndConditionForm
             applicationFormData={applicationFormData}
             setApplicationFormData={setApplicationFormData}
+            applicationFormErrors={applicationFormErrors}
           />
         );
       default:
@@ -121,7 +213,13 @@ export default function SplunkRestAPIAccess() {
         <ColumnLayout.Column span={3} style={LeftColumnStyle}>
           <Icon icon="TelstraIcon" props={{ height: '50px', width: '50px' }} />
           <div style={{ marginTop: '24px' }}>
-            <Info totalPrice={activeStepId >= 4 ? '$144.97' : '$134.98'} />
+            {/* <Info totalPrice={activeStepId >= 4 ? '$144.97' : '$134.98'} /> */}
+            <Info
+              totalCost={calculateCost()}
+              applicationFormData={applicationFormData}
+            >
+
+            </Info>
           </div>
         </ColumnLayout.Column>
 
